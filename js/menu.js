18cdba3a5f403912ed27438eb8d903d3e75af489
Alona Melnykova <alonamelnykova@gmail.com>
2021-08-26 18:43:45 +0300

    JS updated

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open"),
     closeMenuBtn: document.querySelector(".menu-close"),
-    menu: document.querySelector("[data-menu]"),
+    menu: document.querySelector(".mobile-menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleModal);
